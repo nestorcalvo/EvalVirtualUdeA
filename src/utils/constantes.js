492234d49f4694759d7f5c2da1c99ada112e5151
Nestor Calvo <nestor.calvo@udea.edu.co>
2021-10-26 08:25:59 -0500

    v1.3.2

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,4 +1,4 @@
-export const VERSION = "v1.3.1";
+export const VERSION = "v1.3.2";
 // COMMON URL
 // export const URL_BIOMETRICS = 'https://api.ingeniaudea.co/evalvirtual/webapi'
 // export const URL_BIOMETRICS = 'http://arquimedes.udea.edu.co:8098'
